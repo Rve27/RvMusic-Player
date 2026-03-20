efc50a28969bd6ec4efc5ab55bd3c29dc4e071a2
Rve27 <rve27github@gmail.com>
2026-03-20 10:51:11 +0000

    CrashReport: PixelPlayer -> RvMusic Player
    Signed-off-by: Rve27 <rve27github@gmail.com>

--- app/src/main/java/com/rve/musicplayer/presentation/components/CrashReportDialog.kt
+++ app/src/main/java/com/rve/musicplayer/presentation/components/CrashReportDialog.kt
@@ -165,7 +165,7 @@ fun CrashReportDialog(
                         onClick = {
                             val shareIntent = Intent(Intent.ACTION_SEND).apply {
                                 type = "text/plain"
-                                putExtra(Intent.EXTRA_SUBJECT, "PixelPlayer Crash Report")
+                                putExtra(Intent.EXTRA_SUBJECT, "RvMusic Player Crash Report")
                                 putExtra(Intent.EXTRA_TEXT, crashLog.getFullLog())
                             }
                             context.startActivity(Intent.createChooser(shareIntent, "Share crash report"))
